2242413b8d4ce2df21858ccf4438544f97488cc5
Gabriel Gabriel Alison Junho <n134401@gruposantander.com>
2023-06-28 16:43:10 -0300

    feat: cria endpoint para retornar renda usuario

--- clientes/src/controllers/CardsController.js
+++ clientes/src/controllers/CardsController.js
@@ -1,23 +1,48 @@
 import Users from '../models/User.js';
 
+const validaCard = async (dadosUser, dadosBody) => {
+  const objetosKeys1 = Object.keys(dadosUser);
+  const objetosKeys2 = Object.keys(dadosBody);
+  const objetosValues1 = Object.values(dadosUser);
+  const objetosValues2 = Object.values(dadosBody);
+
+  const resulQntKeys = [];
+  objetosKeys1.forEach(((key) => {
+    if (objetosKeys2.includes(key)) {
+      resulQntKeys.push(key);
+    }
+  }));
+
+  const resulQntValues = [];
+  objetosValues1.forEach(((value) => {
+    if (objetosValues2.includes(value)) {
+      resulQntValues.push(value);
+    }
+  }));
+  const we = resulQntKeys.length === 4;
+  const re = resulQntValues.length === 4;
+
+  if (we && re) {
+    return true;
+  }
+  return false;
+};
+
 class CardController {
   static validateCard = async (req, res) => {
     try {
       const dadosDoCartao = req.body;
-      console.log(dadosDoCartao);
-      const resp = await Users.find();
-      //   console.log(dadosDoCartao);
-      const ensureCard = resp.filter(((user) => user.dadosDoCartao === dadosDoCartao));
-      // aqui acho que são 3 = tb
-      console.log(ensureCard);
-      if (!ensureCard) {
+      const users = await Users.find();
+      const userId = users.filter((async (user) => {
+        if (await validaCard(user.dadosDoCartao, dadosDoCartao)) {
+          return user;
+        }
+      }));
+      if (userId.length === 0) {
         res.status(400).json({ message: 'Invalid Card' });
       }
 
-      // const idCliente = ensureCard.id
-      // console.log(idCliente)
-
-      res.status(200).json('OK validate');
+      res.status(200).json({ id: userId[0]._id });
     } catch (err) {
       res.status(500).json(err);
     }
@@ -25,8 +50,9 @@ class CardController {
 
   static getRentById = async (req, res) => {
     try {
-      //   const resp = await Users.find({}, { dadosDoCartao: 0 });
-      res.status(200).json('OK Rent');
+      const { id } = req.params;
+      const resp = await Users.findById(id);
+      res.status(200).json({ rent: resp.rendaMensal });
     } catch (err) {
       res.status(500).json(err);
     }
