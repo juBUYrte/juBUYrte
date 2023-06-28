2596a66b738cfdb2dbcc8be11fe903afa92251e2
Araujo De Araujo Lauro Bezerra De <n134405@gruposantander.com>
2023-06-28 18:49:31 -0300

    Linter feito, verificação de cliente pelo microserviço feita

--- anti-fraude/src/util/fetchClients.js
+++ anti-fraude/src/util/fetchClients.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 const CLIENTS_HOSTNAME = 'localhost';
 const CLIENTS_PORT = '3001';
-const CLIENTS_URL = `http://${CLIENTS_HOSTNAME}:${CLIENTS_PORT}/api/admin/users`
+const CLIENTS_URL = `http://${CLIENTS_HOSTNAME}:${CLIENTS_PORT}/api/admin/users`;
 
-const fetchClient = async(clientId) => {
-    try {
-        const url = `${CLIENTS_URL}/${clientId}`;
-        const response = await axios.get(url);
-        if(response.status !== 200) {
-            throw new Error(`${response.status} : ${response.data.message}`);
-        }
-
-        return response.data;
-    } catch (err) {
-        throw new Error(err.message);
+const fetchClient = async (clientId) => {
+  try {
+    const url = `${CLIENTS_URL}/${clientId}`;
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      throw new Error(`${response.status} : ${response.data.message}`);
     }
+
+    return response.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
-export { fetchClient };
+export default fetchClient;
