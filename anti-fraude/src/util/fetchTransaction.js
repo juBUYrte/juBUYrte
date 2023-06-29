e73a793297d83f947ac3082b86855cc77f6214ad
Araujo De Araujo Lauro Bezerra De <n134405@gruposantander.com>
2023-06-29 11:55:36 -0300

    FIX: Port e mudando método para PATCH

--- anti-fraude/src/util/fetchTransaction.js
+++ anti-fraude/src/util/fetchTransaction.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const TRANSACTION_HOSTNAME = 'localhost';
-const TRANSACTION_PORT = '3001';
+const TRANSACTION_PORT = '3002';
 const TRANSACTION_URL = `http://${TRANSACTION_HOSTNAME}:${TRANSACTION_PORT}/api/admin/transactions`;
 
 const updateTransaction = async (transactionId, status) => {
   try {
     const url = `${TRANSACTION_URL}/${transactionId}`;
     const dataBody = { status };
-    const response = await axios.put(url, dataBody);
+    const response = await axios.patch(url, dataBody);
     if (response.status !== 200) {
       throw new Error(`${response.status} : ${response.data.message}`);
     }
