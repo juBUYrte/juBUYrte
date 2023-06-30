c5a39d0b831b9220672fde84465d4d5af973d63c
FilipeGarroteDev <filipe.garrote@gmail.com>
2023-06-29 22:49:13 -0300

    chore: implementada validação de clientId e transactionId dentro do mongoose schema.

--- anti-fraude/src/models/Analysis.js
+++ anti-fraude/src/models/Analysis.js
@@ -1,14 +1,39 @@
+/* eslint-disable import/no-relative-packages */
 import mongoose from 'mongoose';
+import User from '../../../clientes/src/models/User.js';
+import Transaction from '../../../transacoes/src/models/Transaction.js';
 
 const analysisSchema = new mongoose.Schema(
   {
     clientId: {
       type: String,
       required: true,
+      validate: {
+        async validator(val) {
+          try {
+            const validClient = await User.findById(val);
+            return validClient;
+          } catch (error) {
+            return false;
+          }
+        },
+        message: 'O id do cliente deve ser um id válido, ou seja, deve ser referente a um cliente existente.',
+      },
     },
     transactionId: {
       type: String,
       required: true,
+      validate: {
+        async validator(val) {
+          try {
+            const validTransaction = await Transaction.findById(val);
+            return validTransaction;
+          } catch (error) {
+            return false;
+          }
+        },
+        message: 'O id da transação deve ser um id válido, ou seja, deve ser referente a uma tranação existente.',
+      },
     },
     status: {
       type: String,
--- anti-fraude/tests/integration/analysis.test.js
+++ anti-fraude/tests/integration/analysis.test.js
@@ -3,8 +3,12 @@ import supertest from 'supertest';
 import app from '../../src/app.js';
 import cleanAnalysisDB from '../utils.js';
 
-const port = 3000;
-const server = app.listen(port);
+let server;
+
+beforeAll(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
 
 afterEach(async () => {
   await cleanAnalysisDB();
@@ -16,6 +20,16 @@ afterAll(async () => {
 
 const request = supertest(app);
 
-describe('POST api/admin/analysis - Criação de análise', () => {
+const validAnalysis = {
+  clientId: 'idQualquer',
+  transactionId: 'idQualquer',
+  status: 'Em Análise',
+};
 
+describe('POST api/admin/analysis - Criação de análise', () => {
+  it('Deve responder com status 401, caso não seja fornecido o token de autenticação', async () => {
+    const response = await request.post('/api/admin/analysis').send(validAnalysis);
+
+    expect(response.status).toBe(401);
+  });
 });
